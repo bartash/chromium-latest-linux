eed51c11ae1cadf068623a0133a02ea40e18b207
Andrew Sherman <asherman@cloudera.com>
2019-06-28 16:07:41 -0700

    better doagnostics

--- delete_old.sh
+++ delete_old.sh
@@ -2,6 +2,6 @@ TMP=/tmp/do$$
 ls -t | egrep '^[[:digit:]]*$' > $TMP
 COUNT=`wc -l $TMP | awk '{print $1}'`
 DEL=`expr $COUNT - 1`
-echo COUNT=$COUNT
-echo DEL=$DEL
+echo found $COUNT chromium distributions
+echo deleting $DEL old distributions
 tail -$DEL $TMP | xargs rm -rf
